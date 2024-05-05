129a53df4d6df09c2940a5fc485a109b1769102d
Mvanegas1505 <mvanegaso1@eafit.edu.co>
2024-05-05 11:34:17 -0500

    Tarea Metodo MyName

---
+++ main.java
@@ -0,0 +1,12 @@
+public class main {
+
+    public static void MyName(){
+        System.out.println("Assignment 3 by Martin Vanegas Ospina");
+    }
+    public static void main(String[] args) {
+
+        MyName();
+
+
+    }
+}
